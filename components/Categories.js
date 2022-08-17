ebc9be2bda805d45fef3e44071d409752abb25a5
Acdaling <38664776+AccieTheKing@users.noreply.github.com>
2022-08-17 13:22:24 +0200

    Updated packages used for expo and added some more data points

--- components/Categories.js
+++ components/Categories.js
@@ -1,20 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { ScrollView } from 'react-native';
+import CategoryData from '../data/category';
 import CategoryCard from './CategoryCard';
 
 const Categories = () => {
+  const [categories] = useState(CategoryData);
+
   return (
     <ScrollView
       horizontal
       showsHorizontalScrollIndicator={false}
       contentContainerStyle={{ paddingHorizontal: 15, paddingTop: 10 }}
     >
-      <CategoryCard imgUrl={'https://picsum.photos/200'} title={'Testing'} />
-      <CategoryCard imgUrl={'https://picsum.photos/200'} title={'Testing'} />
-      <CategoryCard imgUrl={'https://picsum.photos/200'} title={'Testing'} />
-      <CategoryCard imgUrl={'https://picsum.photos/200'} title={'Testing'} />
-      <CategoryCard imgUrl={'https://picsum.photos/200'} title={'Testing'} />
-      <CategoryCard imgUrl={'https://picsum.photos/200'} title={'Testing'} />
+      {categories.map((category, index) => (
+        <CategoryCard imgUrl={category.imgUrl} title={category.title} key={index} />
+      ))}
     </ScrollView>
   );
 };
--- data/category.js
+++ data/category.js
@@ -1,4 +1,32 @@
-export default {
-  name: 'Category',
-  data: [{}],
-};
+export default function CategoryData() {
+  return [
+    {
+      title: 'Offers',
+      imgUrl: 'https://picsum.photos/200',
+    },
+    {
+      title: 'Pizza',
+      imgUrl: 'https://picsum.photos/200',
+    },
+    {
+      title: 'Thai',
+      imgUrl: 'https://picsum.photos/200',
+    },
+    {
+      title: 'Sushi',
+      imgUrl: 'https://picsum.photos/200',
+    },
+    {
+      title: 'Asian',
+      imgUrl: 'https://picsum.photos/200',
+    },
+    {
+      title: 'Italian',
+      imgUrl: 'https://picsum.photos/200',
+    },
+    {
+      title: 'Lebanese',
+      imgUrl: 'https://picsum.photos/200',
+    },
+  ];
+}
